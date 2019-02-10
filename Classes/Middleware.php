4c0dc384c618467c11f219ab0f3e320ade8c31d5
Fini21 <eelis.lynne@gmail.com>
2019-02-10 19:57:14 +0200

    because middleware

--- Classes/Middleware.php
+++ Classes/Middleware.php
@@ -5,13 +5,25 @@ namespace App;
 class Middleware {
     private static $handlers = [];
 
+    public static function user() {
+        return self::$user;
+    }
+
     public static function handle($ware = '') {
         if ($ware == '')
             return true;
 
         foreach (self::$handlers as $name => $func) {
             if ($name == $ware) {
-                return $func->__invoke();
+                $ret = $func->__invoke();
+                if ($ret == null)
+                    return false;
+                
+                if (is_bool($ret))
+                    return $ret;
+
+                self::$user = $ret;
+                return true;
             }
         }
 

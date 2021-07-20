3d95d4516737387c6c5d6e0d6ef50bf8adabfe94
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2021-07-20 10:45:28 +0100

    volatileKV

--- Classes/Config.php
+++ Classes/Config.php
@@ -1,16 +1,48 @@
 <?php
 namespace App;
 
-class Config {
+interface IKeyValueEngine {
+    public function get($key);
+    public function set($key, $value);
+    public function delete($key);
+    public function clear();
+}
+class VolatileKeyValueEngine implements IKeyValueEngine {
+    private $data = array();
+    public function get($key) {
+        if (array_key_exists($key, $this->data)) {
+            return $this->data[$key];
+        }
+        return null;
+    }
+    public function set($key, $value) {
+        $this->data[$key] = $value;
+    }
+    public function delete($key) {
+        unset($this->data[$key]);
+    }
+    public function clear() {
+        $this->data = array();
+    }
+}
+class KeyValue {
 
-	private static $store = [];
+	private IKeyValueEngine $store;
 
-	public static function apply(string $key, mixed $val)
-	{
-		self::$store[$key] = $val;
-	}
-	public static function get($key)
-	{
-		return self::$store[$key];
-	}
+	// constructor
+    public function __construct(IKeyValueEngine $engine) {
+        $this->store = $engine;
+    }
+    public function __get($key) {
+        return $this->store->get($key);
+    }
+    public function __set($key, $value) {
+        $this->store->set($key, $value);
+    }
+    public function __unset($key) {
+        $this->store->delete($key);
+    }
+    public function __clear() {
+        $this->store->clear();
+    }
 }
